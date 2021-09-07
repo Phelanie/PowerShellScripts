b6fdac7d90a4a333bd576b6ad1abdba69f84e8e0
Phelanie <90269711+Phelanie@users.noreply.github.com>
2021-09-07 15:04:23 -0700

    Add files via upload

---
+++ RemoveExpiredADUsers.ps1
@@ -0,0 +1,20 @@
+﻿$CurrentTime = (Get-Date).ToFileTime()
+$CalculatedProperty = @{
+    Label='AccountExpires'
+    Expression = { if (!$_.AccountExpires -or $_.AccountExpires -ge [datetime]::MaxValue.ToFileTime()) {
+                       "Never"
+                   } 
+                   elseif ($_.AccountExpires -le $CurrentTime) {
+                       "Expired"
+                   }
+                   else { 
+                       [datetime]::FromFileTime($_.AccountExpires)
+                   }
+                 }
+}
+
+$users = Get-ADUser -filter "Enabled -eq '$true'" -Properties AccountExpires |
+    Select-Object SamAccountName,Name,$CalculatedProperty
+$users | Where AccountExpires -eq 'Expired' | Foreach-Object {
+    Disable-ADAccount -Identity $_.SamAccountName -WhatIf
+}
